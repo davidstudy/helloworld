69eef2f76726f98853fbb3950d08f0a341bab4f2
dengweize <dengweizeuestc.163.com>
2017-08-08 19:26:07 +0800

    新增:可抽取不同的值，并重新排序

--- deletearray/test.cpp
+++ deletearray/test.cpp
@@ -4,10 +4,18 @@
 
 int main()
 {	
-	int a,b,c,k;
+	int a,b,c,k,r;
+	int m[5];
 	int array[]={0,1,2,3,4,5,6,7,8,9};
 	c=sizeof(array)/sizeof(int);
-	printf("please input a number:");
+	//连续n个取值
+// 	for (int q=0;q<2;q++)
+// 	{
+// 		printf("please input a number:");
+// 		scanf("%d",&a);
+// 	}
+	//只取一个值
+/*	printf("please input a number:");
 	scanf("%d",&a);
 	for (int i=0;i<c;i++)
 	{
@@ -20,6 +28,26 @@ int main()
 			}
 			c--;
 			break;
+		}
+	}	*/
+//要求抽掉几个就几个，取决于y
+	for(int y=0;y<3;y++)
+	{
+		printf("please input a number:");
+		scanf("%d",&m);
+		for (int i=0;i<c;i++)
+		{
+			if (*m==array[i])
+			{
+				k=i;
+				for(k;k<c;k++)
+				{
+					array[k]=array[k+1];
+				}
+				*m=*(m+1);
+				c--;
+				break;
+			}
 		}
 	}
 	for(int j=0;j<c;j++)
