e897c4dc25aca6a3b6fa75b10f533ab440fe51a8
dengweize <dengweizeuestc@163.com>
2017-08-12 10:38:30 +0800

    实现输入学生姓名，学号，性别，分数，并且根据性别判断输出信息，同时将女生的分数总和相加

--- arraytest/arrytest001/test001.cpp
+++ arraytest/arrytest001/test001.cpp
@@ -1,33 +1,36 @@
 #include <STDIO.H>
 #include <IOSTREAM.H>
+#include <STRING.H>
 struct student
 {
 	char name[20];
 	char number[20];
 	char sex[20];
+	int  score;
 };
 int main()
 {
 	student stu[3];
 	int max;
+	int cal1,cal2=0;
+	char str[10]="woman";
 	printf("please enter the number of data\n");
 	scanf("%d",&max);
 	for (int i=0;i<max;i++)
 	{
 		printf("please enter name\n");
-		scanf("%s",&stu[i].name);
-		printf("please enter number\n");
-		scanf("%s",&stu[i].number);
-		printf("please enter sex\n");
-		scanf("%s",&stu[i].sex);
-		printf("***********************\n");
+		scanf("%s %s %s %d",&stu[i].name,&stu[i].number,&stu[i].sex,&stu[i].score);
 	}
-	for (int j=0;j<3;j++)
+	for (int j=0;j<max;j++)
 	{
-		printf("%s\n",stu[j].name);
-		printf("%s\n",stu[j].number);
-		printf("%s\n",stu[j].sex);
-		printf("***********************\n");
-	}
+		if (strcmp(stu[j].sex,str) == 0)  
+		{	
+			cal1 = stu[j].score;
+			cal2 = cal1 + cal2;
+			printf("%s %s %s %d\n",stu[j].name,stu[j].number,stu[j].sex,stu[j].score);
+		} 	
+	}	
+	printf("女生分数总和:%d\n",cal2);
+
 	return 0;
 }
